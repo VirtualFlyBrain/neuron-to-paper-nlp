40ccd1b388aa9213224646cd7fe9932b9d8f2c3f
hkir-dev <huseyin_kir@yahoo.com>
2023-02-07 12:02:28 +0000

    use fbrf identifiers

--- runner.sh
+++ runner.sh
@@ -12,8 +12,8 @@ python3 src/main.py
 
 cd ${ONTOLOGY_FOLDER}
 robot template --template linking.tsv --output linking.owl
-robot template --template publication.tsv --output publication.owl
+#robot template --template publication.tsv --output publication.owl
 
-robot merge -i linking.owl -i publication.owl -o merged.owl
+#robot merge -i linking.owl -i publication.owl -o merged.owl
 
-cp merged.owl "$(basename merged.owl .owl)_$(date +'%Y%m%d%H%M').owl"
+cp linking.owl "$(basename linking.owl .owl)_$(date +'%Y%m%d%H%M').owl"

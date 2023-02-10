53606b989d4a47d2d9ab2afc02654e5079d463af
hkir-dev <huseyin_kir@yahoo.com>
2023-02-10 12:49:37 +0000

    exluded some tests from CI

--- src/test/test_suite.py
+++ src/test/test_suite.py
@@ -2,6 +2,9 @@ import unittest
 
 loader = unittest.TestLoader()
 suite = loader.discover(start_dir='./', pattern='*_test.py')
+# tests that only can run locally (due to memory and time constraints) not on CI action
+local_suite = loader.discover(start_dir='./', pattern='*_localtest.py')
+alltests = unittest.TestSuite((suite, local_suite))
 
 runner = unittest.TextTestRunner()
-runner.run(suite)
+runner.run(alltests)

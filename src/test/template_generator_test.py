854d29372e74c49b5bc0ba36ac4b2abb497708de
hkir-dev <huseyin_kir@yahoo.com>
2023-06-29 10:54:54 +0100

    publication ontology generated

--- src/test/template_generator_test.py
+++ src/test/template_generator_test.py
@@ -14,12 +14,17 @@ class RobotTemplateGeneratorCase(unittest.TestCase):
         if os.path.isfile(PUBLICATION_TEMPLATE):
             os.remove(PUBLICATION_TEMPLATE)
 
-    # def test_publications_template(self):
-    #     generate_publications_robot_template(DATA_FOLDER, PUBLICATION_TEMPLATE)
-    #     table = read_csv_to_dict(PUBLICATION_TEMPLATE, delimiter="\t")[1]
-    #
-    #     self.assertEqual(8, len(table.keys()))  # including robot template row
-    #     self.assertTrue("https://doi.org/10.1038/s41467-019-12758-6" in table.keys())
-    #     data = table["https://doi.org/10.1038/s41467-019-12758-6"]
-    #     self.assertEqual("PMC6800437", data["PMCID"])
-    #     self.assertEqual("Shunfan Wuet al., 2019", data["Label"])
+    def test_publications_template(self):
+        generate_publications_robot_template(DATA_FOLDER, PUBLICATION_TEMPLATE)
+        table = read_csv_to_dict(PUBLICATION_TEMPLATE, delimiter="\t")[1]
+
+        self.assertEqual(8, len(table.keys()))  # including robot template row
+        self.assertTrue("http://flybase.org/reports/FBrf0254421" in table.keys())
+        data = table["http://flybase.org/reports/FBrf0254421"]
+        self.assertEqual("PMC6800437", data["PMCID"])
+        self.assertEqual("Shunfan Wuet al., 2019, Nature Communications", data["Label"])
+        self.assertEqual("owl:NamedIndividual", data["TYPE"])
+        self.assertEqual("Drosulfakinin signaling in fruitless circuitry antagonizes P1 neurons to regulate sexual arousal in Drosophila", data["title"])
+        self.assertEqual("FBrf0254421", data["FlyBase"])
+        self.assertEqual("pub", data["nodeLabel"])
+        self.assertEqual("10.1038/s41467-019-12758-6", data["DOI"])
